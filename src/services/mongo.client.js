7417e1acb8bb003855e555373718d825ff82bec9
agustinpippo <100004503+agustinpfr1@users.noreply.github.com>
2025-10-06 20:58:57 -0300

    mongodb 2

--- src/services/mongo.client.js
+++ src/services/mongo.client.js
@@ -3,12 +3,10 @@ const mongoose = require("mongoose");
 const connect = async () => {
   const {
     MONGODB_CONNECTION_STRING,
-    MONGODB_DB_NAME,
     MONGODB_SERVER_SELECTION_TIMEOUT
   } = process.env;
   try {
     await mongoose.connect(MONGODB_CONNECTION_STRING, {
-      dbName: MONGODB_DB_NAME,
       serverSelectionTimeoutMS: Number(MONGODB_SERVER_SELECTION_TIMEOUT),
     });
     console.log("Connected to MongoDB");
