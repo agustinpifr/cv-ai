ec5bb460ad5d8a1ad25c1bb8bcb2c0fd6314b865
Agustin <agustin@novoid.com>
2025-10-09 15:33:51 -0300

    deploy

--- src/services/instrument.js
+++ src/services/instrument.js
@@ -3,7 +3,5 @@ const Sentry = require("@sentry/node");
 
 Sentry.init({
   dsn: "https://4cd84c7696baca85f06987b6d6ab655d@o4510121274507264.ingest.us.sentry.io/4510121276080128",
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
   sendDefaultPii: true,
 });
